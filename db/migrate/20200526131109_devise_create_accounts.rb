5293ab96a3b34c922907ad648d68735bd38ccc12
nenekelvin <karvinmitchar@gmail.com>
2020-05-26 16:19:55 +0300

    run database migration

--- db/migrate/20200526131109_devise_create_accounts.rb
+++ db/migrate/20200526131109_devise_create_accounts.rb
@@ -5,6 +5,9 @@ class DeviseCreateAccounts < ActiveRecord::Migration[6.0]
     create_table :accounts do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :username,           null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
