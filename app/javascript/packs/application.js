6a588f2efac1eb5a5ca9c6b378094119d0628187
nenekelvin <karvinmitchar@gmail.com>
2020-06-09 20:30:34 +0300

    include jsx

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/activestorage").start()
 
 require("bootstrap")
 
-require("../includes/slides.js")
+require("../includes/slides.jsx")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
