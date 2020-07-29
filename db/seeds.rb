823177f1f176ad57bcb3e93c061750b690b2df59
nenekelvin <karvinmitchar@gmail.com>
2020-07-29 19:29:09 +0300

    typo fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Account.create([
-  {first_name: "Kelvin", last_name: "Munene", username: "mitch", email: "karvin@user.com", password: "123456"},
-  {first_name: "Sarah", last_name: "Kendi", username: "Munene", email: "sarah@user.com", password: "123456"},
-  {first_name: "Andy", last_name: "Jones", username: "ajones", email: "andy@user.com", password: "123456"},
-  {first_name: "Alex", last_name: "Baby", username: "ssmith", email: "alex@user.com", password: "123456"},
-])AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+  {first_name: "Kelvin", last_name: "Munene", username: "mitch", email: "karvin@example.com", password: "123456"},
+  {first_name: "Sarah", last_name: "Kendi", username: "Munene", email: "sarah@example.com", password: "123456"},
+  {first_name: "Andy", last_name: "Jones", username: "ajones", email: "andy@example.com", password: "123456"},
+  {first_name: "Alex", last_name: "Baby", username: "ssmith", email: "alex@example.com", password: "123456"},
+])
